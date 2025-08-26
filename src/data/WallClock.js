06e55affe5b4e73cad50cbcc8ec0eb4cf304c844
akanbi muhammed <muhammedakanbi041@gmail.com>
2025-08-26 14:51:16 +0100

    add all

--- src/data/WallClock.js
+++ src/data/WallClock.js
@@ -1,13 +1,13 @@
-import Clock from "../Bedroom pic/Kitchen/Clock.jpg";
+import Clock2 from "../Bedroom pic/Kitchen/Clock.jpg";
 import Clock1 from "../Categories/Clock/Clock1.jpg";
-import Clock4 from "../Categories/Clock/Clock4.png";
-import Clock3 from "../Categories/Clock/Clock3.png";
+import Clock4 from "../Categories/Clock/clock4.png";
+import Clock3 from "../Categories/Clock/clock3.png";
 export const WallClocks = [
   {
     id: 1,
     
     price: 30000,
-    image: Clock,
+    image: Clock2,
   },
   {
     id: 2,
